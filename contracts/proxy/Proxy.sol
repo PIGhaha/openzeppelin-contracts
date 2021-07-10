f0d72eaaf3ec087a6dc5c02ffb0733dcb500895d
PIGhaha <78994667+PIGhaha@users.noreply.github.com>
2021-07-10 21:22:54 +0800

    Update Proxy.sol

--- contracts/proxy/Proxy.sol
+++ contracts/proxy/Proxy.sol
@@ -23,18 +23,26 @@ abstract contract Proxy {
             // Copy msg.data. We take full control of memory in this inline assembly
             // block because it will not return to Solidity code. We overwrite the
             // Solidity scratch pad at memory position 0.
+            
+            //calldatacopy(t, f, s): 从代码的位置f 开始拷贝s 个字节到内存的位置t
             calldatacopy(0, 0, calldatasize())
 
             // Call the implementation.
             // out and outsize are 0 because we don't know the size yet.
+            
+            //delegatecall(g, a, in, insize, out, outsize) : 使用 mem[in…(in + insize)) 作为输入数据， 提供g gas 
+            //对地址 a 发起消息调用， 输出结果数据保存在 mem[out…(out + outsize))， 发生错误
+            //（比如 gas 不足）时返回 0，正确结束返回 1，保留caller和callvalue
             let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
 
             // Copy the returned data.
+            //returndatacopy(t,f,s): 从returndata的位置 f 拷贝 s 个字节到内存位置t
             returndatacopy(0, 0, returndatasize())
 
             switch result
             // delegatecall returns 0 on error.
             case 0 {
+                //revert(p,s):终止运行，返回men[p...(p+s)]的数据
                 revert(0, returndatasize())
             }
             default {
@@ -54,6 +62,8 @@ abstract contract Proxy {
      *
      * This function does not return to its internall call site, it will return directly to the external caller.
      */
+    //将当前调用委托给_implementation返回的地址
+    //该函数不返回其内部调用点，它会直接返回给外部调用者。
     function _fallback() internal virtual {
         _beforeFallback();
         _delegate(_implementation());
@@ -63,6 +73,7 @@ abstract contract Proxy {
      * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if no other
      * function in the contract matches the call data.
      */
+    // 如果没有其他函数与call data相匹配，则Fallback函数将运行并将调用委托给_implentation()返回的函数
     fallback() external payable virtual {
         _fallback();
     }
@@ -71,6 +82,7 @@ abstract contract Proxy {
      * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if call data
      * is empty.
      */
+    //如果data域为空，则receive将运行并将调用委托给_implemetation()返回的函数
     receive() external payable virtual {
         _fallback();
     }
